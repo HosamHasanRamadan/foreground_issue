1269e83b3e2fed229b126d0476bc28b468252932
HosamHasanRamadan <hosam.hasan.ramadan@gmail.com>
2023-08-11 19:06:53 +0300

    remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'firebase_options.dart';
